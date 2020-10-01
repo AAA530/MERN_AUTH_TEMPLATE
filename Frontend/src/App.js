07fec0cfd8ea989284a1f262a9cb61b97324b0fb
AAA530 <dhairyapatelad@gmail.com>
2020-10-01 18:53:06 +0530

    removed a error

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import Header from "./components/Layout/Header";
 import Home from "./components/Pages/Home";
 
-import UserContext from "./Context/UserContext";
-
 import "./Styles.css";
+// import UserContext from "./Context/UserContext";
+import UserContext from "./Context/UserContext";
+// const UserContext = createContext();
 
 function App() {
 	const [userData, setUserData] = useState({
-		token: undefined,
-		user: undefined,
+		token: 12,
+		user: 12,
 	});
 
 	return (
 		<>
 			<BrowserRouter>
-				<UserContext.provider value={{ userData, setUserData }}>
+				<UserContext.Provider value={{ userData, setUserData }}>
 					<Header />
 					<Switch>
 						<Route path="/login" component={Login} />
 						<Route path="/register" component={Register} />
 						<Route path="/" component={Home} />
 					</Switch>
-				</UserContext.provider>
+				</UserContext.Provider>
 			</BrowserRouter>
 		</>
 	);
