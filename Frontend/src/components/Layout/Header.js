8bca1313d709c36eff49926f8d75e89ac4acae07
AAA530 <dhairyapatelad@gmail.com>
2020-12-06 00:01:03 +0530

    added forms for login and register

--- Frontend/src/components/Layout/Header.js
+++ Frontend/src/components/Layout/Header.js
@@ -37,7 +37,7 @@ function Header() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            Template
           </Typography>
 
           <AuthOption />
