be5f720e4560d24e326ced6c93e2258030d53af1
AAA530 <dhairyapatelad@gmail.com>
2020-12-05 20:09:34 +0530

    resuming

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -8,59 +8,59 @@ import Axios from "axios";
 
 import "./Styles.css";
 // import UserContext from "./Context/UserContext";
-import UserContext from "./Context/UserContext";
+// import UserContext from "./Context/UserContext";
 // const UserContext = createContext();
 
 function App() {
-	const [userData, setUserData] = useState({
-		token: undefined,
-		user: undefined,
-	});
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
 
-	useEffect(() => {
-		const checkLoggedin = async () => {
-			const token = localStorage.getItem("auth-token");
-			if (token === null) {
-				localStorage.setItem("auth-token", "");
-				token = "";
-			}
-			const tokenRes = await Axios.post(
-				"http://localhost:5000/users/tokenIsValid",
-				null,
-				{ headers: { "x-auth-token": token } }
-			);
+  useEffect(() => {
+    const checkLoggedin = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+      const tokenRes = await Axios.post(
+        "http://localhost:5000/users/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
+      );
 
-			if (tokenRes) {
-				const userRes = await Axios.get("http://localhost:5000/users", {
-					headers: { "x-auth-token": token },
-				});
-				console.log(userRes);
-				setUserData({
-					token,
-					user: userRes.data,
-				});
-			}
+      if (tokenRes) {
+        const userRes = await Axios.get("http://localhost:5000/users", {
+          headers: { "x-auth-token": token },
+        });
+        console.log(userRes);
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+      }
 
-			console.log(tokenRes.data);
-		};
+      console.log(tokenRes.data);
+    };
 
-		checkLoggedin();
-	}, []);
+    checkLoggedin();
+  }, []);
 
-	return (
-		<>
-			<BrowserRouter>
-				<UserContext.Provider value={{ userData, setUserData }}>
-					<Header />
-					<Switch>
-						<Route path="/login" component={Login} />
-						<Route path="/register" component={Register} />
-						<Route path="/" component={Home} />
-					</Switch>
-				</UserContext.Provider>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter>
+        {/* <UserContext.Provider value={{ userData, setUserData }}> */}
+        <Header />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/" component={Home} />
+        </Switch>
+        {/* </UserContext.Provider> */}
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
